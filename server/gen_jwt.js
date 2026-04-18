72c16df856eb6e79c8bbccb265716a24c257bbdc
ItsMat78 <shreyashrai078@gmail.com>
2026-04-19 03:12:50 +0530

    refactor: remove obsolete automation scripts and temporary test files

--- server/gen_jwt.js
+++
@@ -1,3 +0,0 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config({ path: 'e:/Projects/Minor_management/server/.env' });
-console.log(jwt.sign({ id: 'someId' }, process.env.JWT_SECRET || 'secret'));
--- server/inspect_targetbatch.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/minor_management').then(async () => {
-    const db = mongoose.connection.db;
-    const groups = await db.collection('groups').find({ targetBatch: { $exists: true } }).toArray();
-    console.log(JSON.stringify(groups, null, 2));
-    process.exit(0);
-});
